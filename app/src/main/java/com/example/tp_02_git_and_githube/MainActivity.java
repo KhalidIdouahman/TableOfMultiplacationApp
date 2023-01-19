0da20c0d922590187750974ea3519034ba762fa4
khalid <khalidcourses2022@gmail.com>
2023-01-19 20:57:42 +0100

    colors buttons add and editText centered

--- app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
+++ app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tp_02_git_and_githube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +11,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_display, btn_reset,btn_leave;
+    Button btn_white, btn_lavender,btn_pink;
     EditText input_text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         btn_display = findViewById(R.id.btn_display);
         btn_leave = findViewById(R.id.btn_leave);
         input_text = findViewById(R.id.input_text);
+
+        btn_white = findViewById(R.id.btn_white);
+        btn_lavender = findViewById(R.id.btn_lavender);
+        btn_pink = findViewById(R.id.btn_pink);
+
+//        btn_white.setBackg;
+//        btn_lavender.setBackground(Drawable.createFromPath("lavender"));
+//        btn_pink.setBackground(Drawable.createFromPath("pink"));
 
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
