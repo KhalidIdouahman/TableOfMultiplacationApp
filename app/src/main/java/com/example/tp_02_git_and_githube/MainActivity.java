a28ba4938272dead169a5fe2b2c283f568e11143
khalid <khalidcourses2022@gmail.com>
2023-01-19 10:15:34 +0100

    button reset and leave are developed

--- app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
+++ app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.tp_02_git_and_githube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_display, btn_reset,btn_leave;
+    EditText input_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_reset = findViewById(R.id.btn_reset);
+        btn_display = findViewById(R.id.btn_display);
+        btn_leave = findViewById(R.id.btn_leave);
+        input_text = findViewById(R.id.input_text);
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input_text.setText("");
+            }
+        });
+        btn_leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
