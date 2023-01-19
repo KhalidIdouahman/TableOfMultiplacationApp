676479d01e9a23d1acac9f0f57c1a28097edebcb
khalid <khalidcourses2022@gmail.com>
2023-01-19 23:07:37 +0100

    resolving bugs exported from master and fixed conflicts

--- app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
+++ app/src/main/java/com/example/tp_02_git_and_githube/MainActivity.java
@@ -1,22 +1,19 @@
 package com.example.tp_02_git_and_githube;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
-
-import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     View layout;
     Button btn_display, btn_reset,btn_leave;
     Button btn_white, btn_lavender,btn_pink;
     EditText input_text;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +25,23 @@ public class MainActivity extends AppCompatActivity {
         btn_reset = findViewById(R.id.btn_reset);
         btn_display = findViewById(R.id.btn_display);
         btn_leave = findViewById(R.id.btn_leave);
+
         input_text = findViewById(R.id.input_text);
 
         btn_white = findViewById(R.id.btn_white);
         btn_lavender = findViewById(R.id.btn_lavender);
         btn_pink = findViewById(R.id.btn_pink);
 
+        textView = findViewById(R.id.textView_display);
+
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 input_text.setText("");
+                textView.setText("");
+                for (int i = 0; i < 11 ; i++) {
+                    textView.append(multiplicationDisplay("?",i,"?\n"));
+                }
             }
         });
         btn_leave.setOnClickListener(new View.OnClickListener() {
@@ -51,4 +55,15 @@ public class MainActivity extends AppCompatActivity {
         btn_lavender.setOnClickListener(view -> layout.setBackgroundColor(Color.rgb(241 ,151 ,255)));
         btn_pink.setOnClickListener(view -> layout.setBackgroundColor(Color.rgb(253 ,155 ,206)));
     }
+
+    public String multiplicationDisplay(String place1 , int place2, String place3) {
+        return String.format("%s * %d = %s",place1,place2,place3);
+    }
+
+//    textView.setText("");
+//                for (int i = 0; i < 11 ; i++) {
+//        int y = 4;
+//        int x = y*i;
+//        textView.append(multiplicationDisplay(Integer.toString(y),i,Integer.toString(x))+"\n");
+//    }
 }
